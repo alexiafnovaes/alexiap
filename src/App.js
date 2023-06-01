c69540466434c0535fd332f5476d40e1a3438920
alexiafnovaes <135263369+alexiafnovaes@users.noreply.github.com>
2023-06-01 14:58:18 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import logo from './logo.svg';
+import './App.css';
+import url from './alexiaicone.jpeg';
+import lex from './components/CURRÍCULO.pdf';
+import ap from './components/AjudandoPlanetas.apk';
+import app from './components/CasoDiario.apk';
+import appp from './components/Figurantes.apk';
+import apppp from './components/MIAU.apk';
+import appppp from './components/VendasIlegais.apk';
+import dut from './components/fotocr.jpeg';
+
+function App() {
+  
+  return (
+    <div className="App">
+
+
+      
+
+      <div class="bnt">
+        <section class="proj">
+          
+          
+        <h1>Aplicativos Desenvolvidos:</h1>
+        
+        <p> </p>
+      <h3>Venda de orgãos</h3>
+      <a  id="av" href={appppp}>-  Fazer download  -</a>
+      <h3>Figurantes de Velório</h3>
+      <a  id="av" href={appp}>-  Fazer download  -</a>
+      <h3>Caso Diário</h3>
+      <a  id="av" href={app}>-  Fazer download  -</a>
+      <h3>MIAU – Gatos incluidos na  Sociedade</h3>
+      <a  id="av" href={apppp}>-  Fazer download  -</a>
+      <h3>Ajudando Planetas </h3>
+      <a  id="av" href={ap}>-  Fazer download  -</a>
+      <p> </p>
+      <p><img class="farofe" src="https://s2.glbimg.com/mTOxpglY5vPGVghs4JD4fihcVbo=/0x0:620x443/600x0/smart/filters:gifv():strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/Y/5/iF8OmoTy6eQdkT9Xjz5g/2012-11-05-fundo-transparente.png"></img></p>
+      <img class="farofo" src="https://i.pinimg.com/originals/06/64/51/066451b414eb74cce52d1c7a7c28d183.gif"></img>
+      <img class="farofo" src="https://i.pinimg.com/originals/06/64/51/066451b414eb74cce52d1c7a7c28d183.gif"></img>
+      <img class="farofo" src="https://i.pinimg.com/originals/06/64/51/066451b414eb74cce52d1c7a7c28d183.gif"></img>
+      <img class="farofo" src="https://i.pinimg.com/originals/06/64/51/066451b414eb74cce52d1c7a7c28d183.gif"></img>
+          
+
+        </section>
+
+        <div class="Hom" >
+        <img class="farofa" src="https://i.pinimg.com/originals/99/26/be/9926be4bd26e4feb4c3e2cbb32c6b781.gif"></img>
+
+      <h1><p></p>
+        
+        Alexia Ferreira Novaes</h1>
+      <h3>contatoalexianovaes@gmail.com</h3>
+      <img class="fotinha" src={url}></img>
+      <p> </p>
+      
+      <a  id="cv" href={lex}>CURRÍCULO</a>
+      </div>
+
+        <section class="proj">
+          
+        <h1>Profissões das horas não vagas:</h1>
+        <p> </p>
+      <h3>Youtuber</h3>
+      <a  class="branco"id="https://www.youtube.com/@NovaesAlexia" link href="https://www.youtube.com/@NovaesAlexia">- Ir assistir o flop -</a>
+      <h3>Instagramer</h3>
+      <a  class="branco"id="https://www.instagram.com/alexia.novaes/" link href="https://www.instagram.com/alexia.novaes/">- Ir assistir o flop -</a>
+      <h3>Tiktoker</h3>
+      <a  class="branco"id="https://www.tiktok.com/@alexia.novaes" link href="https://www.tiktok.com/@alexia.novaes">- Ir assistir o flop -</a>
+      <h3>Picadora de Papéis Profissiona</h3>
+      <a  class="branco"id="https://thumbs.dreamstime.com/z/empres%C3%A1ria-malvada-segurando-tesouras-amea%C3%A7ando-com-chifres-de-dem%C3%B4nio-vermelhos-e-tesoura-cauda-229701562.jpg" link href="https://thumbs.dreamstime.com/z/empres%C3%A1ria-malvada-segurando-tesouras-amea%C3%A7ando-com-chifres-de-dem%C3%B4nio-vermelhos-e-tesoura-cauda-229701562.jpg">- Também fiz arte -</a>
+      <h3>Tia da creche</h3>
+      <a  id="av" href={dut}>- Meu real emprego -</a>
+      <p>            </p>
+      <p><img class="farofe" src="https://s2.glbimg.com/mTOxpglY5vPGVghs4JD4fihcVbo=/0x0:620x443/600x0/smart/filters:gifv():strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/Y/5/iF8OmoTy6eQdkT9Xjz5g/2012-11-05-fundo-transparente.png"></img></p>
+      <img class="farofo" src="https://i.pinimg.com/originals/06/64/51/066451b414eb74cce52d1c7a7c28d183.gif"></img>
+      <img class="farofo" src="https://i.pinimg.com/originals/06/64/51/066451b414eb74cce52d1c7a7c28d183.gif"></img>
+      <img class="farofo" src="https://i.pinimg.com/originals/06/64/51/066451b414eb74cce52d1c7a7c28d183.gif"></img>
+      <img class="farofo" src="https://i.pinimg.com/originals/06/64/51/066451b414eb74cce52d1c7a7c28d183.gif"></img>
+      <p> </p>
+
+        </section>
+      </div>
+      
+    </div>
+  );
+}
+export default App;
